1850c329ffb920dfda850b159ef30f3a486ac6fc
Klara <sanchez.clara13@gmail.com>
2019-02-06 12:21:38 +0100

    FileUploader Formated

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -4,30 +4,25 @@ import React from "react";
 import "../css/App.css";
 
 class FileUploader extends React.Component {
-
- previewFile = () => {
-    const preview = document.querySelector("img"); 
-    const file = document.querySelector("input[type=file]").files[0]; 
+  previewFile = () => {
+    const preview = document.querySelector("img");
+    const file = document.querySelector("input[type=file]").files[0];
     const reader = new FileReader();
 
     reader.onloadend = function() {
       preview.src = reader.result;
     };
-
     if (file) {
-      reader.readAsDataURL(file); 
+      reader.readAsDataURL(file);
     } else {
       preview.src = "";
     }
-  }
-
+  };
 
   render() {
-    
-    
     return (
       <form>
-        <input type="file" onChange= {this.previewFile} />
+        <input type="file" onChange={this.previewFile} />
         <img src="" height="200" alt="Preview" />
       </form>
     );
