d3489c4d751ef71967aa7bc6ff080574e8bb13af
Klara <sanchez.clara13@gmail.com>
2019-02-06 12:14:35 +0100

    debugged App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -5,7 +5,6 @@ import FileUpload from "./FileUpload";
 import "../css/App.css";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {};
@@ -20,94 +19,153 @@ class App extends React.Component {
         this.setState({ contacts: response });
       });
   }
+
   handleSubmit() {
     const button = document.querySelector("button");
 
-    //  button.addEventListener("click", event => {
-    //    event.preventDefault();
+    button.addEventListener("click", event => {
+      event.preventDefault();
 
-     const payload = {
-      first_name: this.firstName.current.value,
-      last_name: this.lastName.current.value,
-      mobile_number: this.mobileNumber.current.value,
-      prefix: this.prefix.current.value,
-      phone_number: this.phoneNumber.current.value,
-      email: this.email.current.value,
-      typeOfAcquaintance: this.checkboxes.current.value
-     }
+      const payload = {
+        first_name: this.firstName.current.value,
+        last_name: this.lastName.current.value,
+        mobile_number: this.mobileNumber.current.value,
+        prefix: this.prefix.current.value,
+        phone_number: this.phoneNumber.current.value,
+        email: this.email.current.value,
+        typeOfAcquaintance: this.checkboxes.current.value
+      };
 
-    const url = `http://localhost:4000/app`;
+      const url = `http://localhost:4000/app`;
 
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then(res => res.json())
-      .then(response => console.log("Success:", JSON.stringify(response)))
-      .catch(error => console.error("Error:", error));
-  })
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(res => res.json())
+        .then(response => console.log("Success:", JSON.stringify(response)))
+        .catch(error => console.error("Error:", error));
+    });
+  }
 
-   handleMobileNumber = (e) => {
-       if (/^\d+$/.test(e.target.value)){
-       this.setState({mobileNumber: true});
-       } else {this.setState({mobileNumber: false});
-    } 
-   }
+  import React from "react";
+import FileUpload from "./FileUpload";
 
-   handleFirstName = (e) => {
-     if (e.target.value.length > 4){
-       this.setState({firstName: true});
-     } else {this.setState({firstName: false});
+//Styleshits
+import "../css/App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount() {
+    document.title = "Contact Manager";
+
+    fetch("http://localhost:4000/contacts")
+      .then(resp => resp.json())
+      .then(response => {
+        this.setState({ contacts: response });
+      });
+  }
+
+  handleSubmit() {
+    const button = document.querySelector("button");
+
+    button.addEventListener("click", event => {
+      event.preventDefault();
+
+      const payload = {
+        first_name: this.firstName.current.value,
+        last_name: this.lastName.current.value,
+        mobile_number: this.mobileNumber.current.value,
+        prefix: this.prefix.current.value,
+        phone_number: this.phoneNumber.current.value,
+        email: this.email.current.value,
+        typeOfAcquaintance: this.checkboxes.current.value
+      };
+
+      const url = `http://localhost:4000/app`;
+
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(res => res.json())
+        .then(response => console.log("Success:", JSON.stringify(response)))
+        .catch(error => console.error("Error:", error));
+    });
+  }
+  handleMobileNumber = e => {
+    if (/^\d+$/.test(e.target.value)) {
+      this.setState({ mobileNumber: true });
+    } else {
+      this.setState({ mobileNumber: false });
     }
-   }
+  };
 
-   handleLastName = (e) => {
-     if (e.target.value.length > 4){
-       this.setState({lastName: true});
-     } else {this.setState({lastName: false});
+  handleFirstName = e => {
+    if (e.target.value.length > 4) {
+      this.setState({ firstName: true });
+    } else {
+      this.setState({ firstName: false });
     }
-   }
+  };
 
-   handlePrefix = (e) => {
-     if (/^\+\d{2}$/.test(e.target.value)){
-       this.setState({prefix: true});
-     } else {this.setState({prefix: false});
+  handleLastName = e => {
+    if (e.target.value.length > 4) {
+      this.setState({ lastName: true });
+    } else {
+      this.setState({ lastName: false });
     }
-   }
+  };
 
-   handlePhoneNumber = (e) => {
-     if (/^\d+$/.test(e.target.value)){
-       this.setState({phoneNumber: true});
-     } else {this.setState({phoneNumber: false});
+  handlePrefix = e => {
+    if (/^\+\d{2}$/.test(e.target.value)) {
+      this.setState({ prefix: true });
+    } else {
+      this.setState({ prefix: false });
     }
-   }
+  };
 
-   handleEmail = (e) => {
-    if (e.target.value.length > 5){
-       this.setState({email: true});
-     } else {this.setState({email: false});
+  handlePhoneNumber = e => {
+    if (/^\d+$/.test(e.target.value)) {
+      this.setState({ phoneNumber: true });
+    } else {
+      this.setState({ phoneNumber: false });
     }
-   }
-   
+  };
+
+  handleEmail = e => {
+    if (e.target.value.length > 5) {
+      this.setState({ email: true });
+    } else {
+      this.setState({ email: false });
+    }
+  };
+
   render() {
     const typeOfAcquaintance = ["work", "social", "family"];
     const checkboxes = typeOfAcquaintance.map(typeOfAcquaintance => (
-      
       <div className="form-group form-check">
-      <input
-        type="checkbox"
-        className="form-check-input"
-        id={typeOfAcquaintance}
-        name={typeOfAcquaintance}
-      />
-      <label className="form-check-label mr-3" for={typeOfAcquaintance}>
-        {typeOfAcquaintance}
-      </label>
-    </div>
-  ));
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id={typeOfAcquaintance}
+          name={typeOfAcquaintance}
+        />
+        <label className="form-check-label mr-3" for={typeOfAcquaintance}>
+          {typeOfAcquaintance}
+        </label>
+      </div>
+    ));
 
     return (
       <div className="container ext-border">
@@ -115,37 +173,52 @@ class App extends React.Component {
         <h2 className="text-center">Insert your data</h2>
         <form className="form-inline mt-2" onSubmit={this.handleSubmit}>
           <div className="row col-lg-12">
-
             <label>First Name* </label>
             <input
               value="First Name"
-              onChange = {this.handleFirstName}
+              onChange={this.handleFirstName}
               type="text"
-              className = {this.state.firstName ? "form-control mr-2 col-lg-12 is-valid": "form-control mr-2 col-lg-12 is-invalid"}
+              className={
+                this.state.firstName
+                  ? "form-control mr-2 col-lg-12 is-valid"
+                  : "form-control mr-2 col-lg-12 is-invalid"
+              }
               placeholder="First Name"
             />
 
             <label> Last Name </label>
             <input
-              onChange = {this.handleLastName}
+              onChange={this.handleLastName}
               type="text"
-              className= {this.state.lastName ? "form-control  mr-2 col-lg-12 is-valid": "form-control mr-2 col-lg-12 is-invalid"}
+              className={
+                this.state.lastName
+                  ? "form-control  mr-2 col-lg-12 is-valid"
+                  : "form-control mr-2 col-lg-12 is-invalid"
+              }
               placeholder="Last Name"
             />
 
             <label> Mobile Number </label>
-            <input 
-              onChange= {this.handleMobileNumber}
+            <input
+              onChange={this.handleMobileNumber}
               type="text"
-              className={this.state.mobileNumber ? "form-control mr-2 col-lg-12 is-valid":"form-control mr-2 col-lg-12 is-invalid"}
+              className={
+                this.state.mobileNumber
+                  ? "form-control mr-2 col-lg-12 is-valid"
+                  : "form-control mr-2 col-lg-12 is-invalid"
+              }
               placeholder="Mobile Number"
             />
 
             <label> Prefix </label>
             <input
-              onChange = {this.handlePrefix}
+              onChange={this.handlePrefix}
               type="text"
-              className={this.state.prefix ? "form-control mr-2 col-lg-12 is-valid": "form-control mr-2 col-lg-12 is-invalid"}
+              className={
+                this.state.prefix
+                  ? "form-control mr-2 col-lg-12 is-valid"
+                  : "form-control mr-2 col-lg-12 is-invalid"
+              }
               placeholder="Enter in this format +XX"
             />
 
@@ -153,38 +226,46 @@ class App extends React.Component {
             <input
               onChange={this.handlePhoneNumber}
               type="text"
-              className={this.state.phoneNumber ? "form-control-sm  mr-2 col-lg-12 is-valid" : "form-control mr-2 col-lg-12 is-invalid"}
+              className={
+                this.state.phoneNumber
+                  ? "form-control-sm  mr-2 col-lg-12 is-valid"
+                  : "form-control mr-2 col-lg-12 is-invalid"
+              }
               placeholder="Phone Number"
             />
             <label> Email </label>
             <input
-              onChange = {this.handleEmail}
+              onChange={this.handleEmail}
               type="email"
-              className={this.state.email ? "form-control-sm mr-2 col-lg-12 is-valid" : "form-control mr-2 col-lg-12 is-invalid"}
+              className={
+                this.state.email
+                  ? "form-control-sm mr-2 col-lg-12 is-valid"
+                  : "form-control mr-2 col-lg-12 is-invalid"
+              }
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Enter your email"
             />
           </div>
           <h2> Type of Acquaintance </h2>
-         {checkboxes}
+          {checkboxes}
 
-         <FileUpload />
-{ this.state.contacts && this.state.contacts.map(contact => {
-  return ( 
-  <li>{contact.email}</li>)
-})}
-          <div className= "container"> 
-           <button type="button" className="btn btn-dark">
+          <FileUpload />
+          {this.state.contacts &&
+            this.state.contacts.map(contact => {
+              return <li>{contact.email}</li>;
+            })}
+          <div className="container">
+            <button type="button" className="btn btn-dark">
               {" "}
               Send{" "}
             </button>
           </div>
-
         </form>
       </div>
     );
+    
+    export default App;
   }
 }
 
-export default App;
